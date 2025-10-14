51b11446ea360db9c1efae01f5fd7cb6cccb0c83
YareBE <yare.bespinosa@udc.es>
2025-10-14 18:12:00 +0200

    feat:Update interface for data reading

---
+++ src/data_manager.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import sqlite3
+import openpyxl
+
+class DataUploader:
+    """Class responsible for the correct uploading and error handling
+    of the selected file in the interface
+    """
+    def __init__(self, file):
+        self.__file = file
+    
+    def error_handle(self):
+        extension = self.__file.name.split('.')[-1].lower()
+        #Set the pointer of the file-like variable to the beginning
+        assert extension in ('csv', 'xls', 'xlsx', 'db', 'sqlite'), \
+        "ERROR: Invalid extension"
+        self.__file.seek(0) 
+        if extension == 'csv':
+            data = self._upload_csv()
+        elif extension in ('xls', 'xlsx'):
+            data = self._upload_excel()
+        elif extension in ('db', 'sqlite'):
+            data = self._upload_sql()
+        return data
+    
+    def _upload_csv(self):
+        try:
+            data = pd.read_csv(self.__file)
+            return data
+        except pd.errors.EmptyDataError:
+            raise Exception("ERROR: the selected file is empty")
+        
+    def _upload_excel(self):
+        try:
+            data = pd.read_excel(self.__file, engine = 'openpyxl')
+            return data
+        except pd.errors.EmptyDataError:
+            raise Exception("ERROR: the selected file is empty")
+    
+    def _upload_sql(self, file):
+        conn = sqlite3.connect(':memory:') 
+        try:
+            conn.deserialize(file.read()) #Instead of a temp file
+            table = pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table' LIMIT 1", conn).iloc[0, 0]
+            data = pd.read_sql_query(f"SELECT * FROM {table}", conn)
+            return data
+        except Exception as err:
+            raise Exception("Error while reading the data:", err)
+        finally:
+            conn.close()
--- src/interface.py
+++ src/interface.py
@@ -1,7 +1,33 @@
 import streamlit as st
+from data_manager import DataUploader
 
-st.title("ModeLine")
-archivo = st.file_uploader(
-    "Selecciona un dataset",
-    type=["csv", "xls", "xlsx", "db", "sqlite"] 
-)
+def data_preview(data):
+    st.dataframe(data.head(10))
+
+
+if __name__ == '__main__':
+    st.title("ModeLine")
+
+    st.session_state.data_file = st.file_uploader(
+        "You can select only 1 file",
+        type=["csv", "xls", "xlsx", "db", "sqlite"] 
+    )
+
+    if "data_file" not in st.session_state:
+        st.badge("You have not selected any file yet", icon=":material/warning:", color="yellow")
+    
+    if st.session_state.data_file is not None:
+        try:
+            whole_file = DataUploader(st.session_state.data_file)
+            st.session_state.dataset = whole_file.error_handle()
+        except Exception as err:
+            st.error(err)
+            file_reload = st.button("RETRY", type = 'primary')
+            if file_reload:
+                del st.session_state['data_file']
+        
+        st.button("DONE", type = 'primary',\
+                 on_click = data_preview(st.session_state.dataset))
+
+
+
