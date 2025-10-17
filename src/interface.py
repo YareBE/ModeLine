191574df9eaa4c28ab1b83c97d3fbfe5d1c3d876
Pablo Fernández <pablo.fernandez.rios@udc.es>
2025-10-17 16:55:18 +0200

    fix(interface.py): fix minor errors when using the interface
    @st.cache_data caused the *load_bar* to never stop running. The columns in upload_file caused a problem when visualizing data, now buttons are in a column and the tables are show in the entire page. The *change_dataset* button now works, but only allows to make a change (it needs to be refactored). A blank *data_display* line was removed. The line st.set_page_config() allows streamlit to show the interface in the entire page, but is not implemented.

--- src/interface.py
+++ src/interface.py
@@ -10,51 +10,62 @@ def data_display(data: pd.DataFrame, type = "preview" or "full"):
         st.dataframe(data)
 
 #cache_data optimizes computation time by storing function results
-@st.cache_data 
 def file_filter(data_file):
     with st.status(label = "Loading data...", state = "running") as status:
             try:
                 selected_file = DataUploader(data_file)
                 dataset = selected_file.error_handler()
+                status.update(label = "Data correctly uploaded",
+                              state="complete")
             except Exception as err:
                 status.update(label = f"Error while reading the file: {err}",
                     state = "error")
                 if st.button("RETRY", type = 'primary'):
                     st.rerun()
-            status.update(label = "blue-background[Data correctly uploaded]",
-                         state = "complete")
     return dataset
     
 def upload_file():
+    # We select a file and save this file in the key 'file 1'
     data_file = st.file_uploader(
         "You can select only 1 file",
-        type=["csv", "xls", "xlsx", "db", "sqlite"] 
+        type=["csv", "xls", "xlsx", "db", "sqlite"],
+        key="file 1"
     )
+
     if data_file is None:
         st.badge("You have not selected any file yet",\
-                  icon=":material/warning:", color="yellow")
+                  icon=":material/warning:", color="yellow",
+                 width="stretch") # Width not working!!!
     else:
         data_file = file_filter(data_file)
         col1, col2, col3 = st.columns([1, 1, 1])
         with col1:
-            if st.checkbox("PREVIEW"):
-                data_display(data_file, "preview")
+            preview = st.checkbox("PREVIEW")
         with col2:
-            if st.button("CHANGE DATASET", type = "primary"):
-                st.rerun()
+            change_dataset = st.button("CHANGE DATASET", type = "primary")
         with col3:
-            if st.button("CONFIRM", type = 'primary'):
-                dataset = data_file
-        
+            confirm = st.button("CONFIRM", type = 'primary')
 
+        if preview:
+            # data_display will show the first rows of the dataset
+            data_display(data_file, "preview")
+        elif change_dataset:
+            # We select a new file and save in 'file 2'
+            data_file = st.file_uploader(
+                    "You can select only 1 file",
+                    type=["csv", "xls", "xlsx", "db", "sqlite"],
+                    key="file 2"
+                    ) # This needs to be refactor so that we can change multiple times of tile, not just one
+        elif confirm:
+            # data_display will show the complete dataset
+            data_display(data_file, "full")
+            st.badge("Dataset correctly processed", icon = ":material/check:",
+                     color = "green")
 
-        
 def main():
     st.title("ModeLine")
+    # st.set_page_config(layout="wide") # Set streamlit width to the entire page
     full_dataset = upload_file()
-    st.badge("Dataset correctly processed", icon = ":material/check:", color = "green")
-    data_display(full_dataset, type = "full")
-
 
 
 if __name__ == '__main__':
